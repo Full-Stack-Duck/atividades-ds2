7f5a5ec39e01ad409aec4fd41183df9404209305
ViniciusDevAssis <viniciusfsa2014@gmail.com>
2023-02-13 20:04:56 -0300

    Magic Square

---
+++ MagicSquareSENAI/facade.py
@@ -0,0 +1,14 @@
+from generator import Generator
+from splitter import Splitter
+from verifier import Verifier
+
+
+class MagicSquareGenerator:
+    def generate(self, size: int):
+        generator = Generator()
+        splitter = Splitter()
+        verifier = Verifier()
+
+        # Código aqui...
+        
+        return square
